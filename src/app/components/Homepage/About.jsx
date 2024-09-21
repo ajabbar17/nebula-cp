f3504709f645eab5819113a895c0e341e446254e
ajabbar17 <ajabbar1234@gmail.com>
2024-09-21 17:31:55 +0500

    removed image

--- src/app/components/Homepage/About.jsx
+++ src/app/components/Homepage/About.jsx
@@ -5,20 +5,18 @@ import React from 'react';
 const About = () => {
   return (
     <section className="w-full gap-16 items-center  flex md:flex-row flex-col md:px-28 md:py-16 py-6 px-6 ">
-      <div className='w-[50%] flex  items-center md:items-start flex-col' >
+      <div className='w-full md:w-[50%] flex  items-center md:items-start flex-col' >
        
 
         
          
         <p className='text-white mt-4 md:mt-10 text-base md:text-lg font-bold'>
           “Access a comprehensive strategy encompassing education, yields, and exclusive early-stage Web3 investments.”</p>
-        <div className='flex gap-4 items-center mt-6'>
+        <div className='flex gap-4 w-full items-center mt-6'>
           
-          <div>
-            <Image src="/mayukh.png" width={100} height={100} className='rounded-full w-20' alt="invest" />
-          </div>
-            <div>
-              <h1 className='text-2xl font-semibold'>
+          
+            <div className='text-start w-full'>
+              <h1 className='text-3xl  font-bold'>
               Mayukh Gon
               </h1>
               <p className='font-semibold uppercase'>
